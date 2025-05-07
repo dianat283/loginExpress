aba2f4f6e0e1a1d1a121f6e2e0c3c75f98a01c48
dianat283 <dia.mike@hotmail.com>
2025-05-07 17:40:02 -0500

    se envia el modulo de aseguradora AA5 EV03

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const app = express(); // Inicializar la aplicación Express
 const port = 5000; // Definir el puerto en el que se ejecutará el servidor
 
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
@@ -90,6 +91,75 @@ app.put('/login', async (req, res) => {
       res.status(500).send('Error en el servidor');
   }
 });
+
+
+//   // Ruta aseguradora para verificar que el servidor está funcionando
+app.get('/test', (req, res) => {
+  res.json({ mensaje: "El servidor está funcionando correctamente" });
+});
+
+app.get('/', (req, res) => {
+res.send('Hola Mundo!'); // Responder con un mensaje simple
+});
+
+// Ruta para manejar inicio de sesión
+app.get('/aseguradoras', async (req, res) => {
+  try {
+      const [results] = await connection.query("SELECT * FROM aseguradora");
+      res.setHeader('Content-Type', 'application/json'); // Establecer encabezado JSON
+      res.json(results);
+  } catch (err) {
+      console.error("Error en la consulta:", err); // Verifica el mensaje exacto en la terminal
+      res.status(500).json({ error: "Error en el servidor" });    
+  }
+  });
+
+// crear un usuario
+app.post('/aseguradoras', async (req, res) => {
+console.log("Datos recibidos:", req.body); // Verifica qué datos están llegando
+const {identificacion, razon_social, cobertura } = req.body;
+
+if (!identificacion || !razon_social || !cobertura) {
+  return res.status(400).json({ error: "Todos los campos son obligatorios" });
+}
+
+try {
+  await connection.query(
+    "INSERT INTO aseguradora (identificacion, razon_social, cobertura) VALUES (?, ?, ?)",
+    [identificacion, razon_social, cobertura]
+  );
+  res.status(201).json({ mensaje: "Aseguradora creada exitosamente" });
+} catch (err) {
+  console.error(err);
+  res.status(500).send('Error en el servidor');
+}
+});
+
+app.get('/validar', (req, res) => {
+res.send('Sesion validada'); // Respuesta de validación exitosa
+});
+
+// Actualizar un usuario
+app.put('/aseguradoras/id', async (req, res) => {
+  const { id } = req.params;
+  const { identificacion, razon_social, cobertura} = req.body;
+
+  if (!identificacion || !razon_social || !cobertura) {
+    return res.status(400).json({ error: "Todos los campos son obligatorios" });
+  }
+
+  try {
+    await connection.query(
+      "UPDATE aseguradora SET identificacion = ?, razon_social = ?, cobertura = ? WHERE id_aseguradora = ?",
+      [identificacion, razon_social, cobertura, id]
+    );
+    res.json({ mensaje: "Aseguradora actualizada correctamente" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error en el servidor');
+  }
+});
+//Fin aseguradora
 
 // Iniciar el servidor 
 app.listen(port, () => {
